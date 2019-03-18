c3c6dc83050d3b0573fcea034785e1d68742dcb1
Harry Seong <harry@northwestern.edu>
2019-03-18 00:21:05 -0500

    Home page styling

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,9 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import {animate, query, sequence, stagger, style, transition, trigger} from '@angular/animations';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
+  animations: [
+    trigger('featureSummaryAnimations', [
+      transition(':enter', [
+        query('.feature', [
+          style({ opacity: 0, transform: 'translateY(1em)'}),
+          stagger(300, [
+            sequence([
+              animate('1s 0.5s ease', style({ opacity: 1, transform: 'translateY(0)' })),
+            ])
+          ]),
+        ])
+      ]),
+    ])
+  ]
 })
 export class HomeComponent implements OnInit {
 
