dfe0bd15349a92fba24dfa8417b12eb2706bc15a
Harry Seong <harry@northwestern.edu>
2019-03-26 12:49:25 -0500

    Updated Explore page navbar

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 import {UserService} from '../../shared/services/user/user.service';
 
 @Component({
@@ -7,6 +7,8 @@ import {UserService} from '../../shared/services/user/user.service';
   styleUrls: ['./navbar.component.scss']
 })
 export class NavbarComponent implements OnInit {
+  @Input()
+  isExplore: boolean;
 
   constructor(public userService: UserService) { }
 
