5600ccb36b7828647ae54b2fa2fe5faaf3209ccc
Harry Seong <harry@northwestern.edu>
2019-05-03 10:55:26 -0700

    Fixed typo for environment variable name.

--- src/shared/services/api/google-books/google-books-api.service.ts
+++ src/shared/services/api/google-books/google-books-api.service.ts
@@ -10,15 +10,15 @@ export class GoogleBooksApiService {
   constructor(private http: HttpClient) { }
 
   lookupBooks(searchTerm: string) {
-    return this.http.get(environment.api.goole_books_url + '?q=' + searchTerm + '&orderBy=relevance&maxResults=30');
+    return this.http.get(environment.api.google_books_url + '?q=' + searchTerm + '&orderBy=relevance&maxResults=30');
   }
   lookupBooksByTitle(title: string) {
-    return this.http.get(environment.api.goole_books_url + '?q=title:' + title);
+    return this.http.get(environment.api.google_books_url + '?q=title:' + title);
   }
   lookupBooksByAuthor(author: string) {
-    return this.http.get(environment.api.goole_books_url + '?q=author:' + author);
+    return this.http.get(environment.api.google_books_url + '?q=author:' + author);
   }
   lookupBooksByIsbn13(isbn13: string) {
-    return this.http.get(environment.api.goole_books_url + '?q=isbn:' + isbn13);
+    return this.http.get(environment.api.google_books_url + '?q=isbn:' + isbn13);
   }
 }
